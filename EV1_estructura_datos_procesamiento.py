9005aec48005e223861c1fb48b58d90bef7cc0fd
Jean Carlos GR <jeangarza12@gmail.com>
2026-08-19 16:49:39 -0600

    1er Ejemplo de lista con metodo

--- EV1_estructura_datos_procesamiento.py
+++ EV1_estructura_datos_procesamiento.py
@@ -1,1 +1,5 @@
-print("PRUEBA DE GIT")
+#3 Ejemplos de listas con diferentes métodos 
+lista = ["Jean", "Carlos", "Salvador"]
+lista.append("Garza")
+print(lista)
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
