71bf57a0b8e6c4c7e4a215d6ddbe7bb40e189ed3
Jean Carlos GR <jeangarza12@gmail.com>
2026-08-19 18:02:48 -0600

    3 Ejemplos de diccionarios

--- EV1_estructura_datos_procesamiento.py
+++ EV1_estructura_datos_procesamiento.py
@@ -12,3 +12,24 @@ print("--------------------------------------------------------------")
 
 lista.extend(["Garza","Rios"])
 print(lista)
+
+#3 EJEMPLOS DE TUPLAS CON DIFERENTES METODOS 
+tupla= (10,100, 1000)
+print(tupla)
+
+
+
+#3 EJEMPLOS DE DICCIONARIOS 
+diccionario = {"a":1, "b":2, "c":3}
+v_items = diccionario.items()
+print(v_items)
+
+#2do ejemplo con keys 
+diccionario = {"a":1, "b":2, "c":3}
+v_keys = diccionario.keys()
+print(v_keys)
+
+#3er ejemplo con valores 
+diccionario = {"a":1, "b":2, "c":3}
+v_valores = diccionario.values()
+print(v_valores)
